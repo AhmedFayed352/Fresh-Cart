936ad3de4e5e2d51a4fba6b45b0571be38bf051f
AhmedFayed352 <fayedahmed352@gmail.com>
2024-07-03 03:34:30 +0200

    editing

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -17,7 +17,7 @@ export class ProductComponent implements OnDestroy{
 
   addItemToCart(id:string) {
     this.arr.push(this._CartService.addToCart(id).subscribe({
-      next: (response) => {response},
+      next: (response) => {console.log(response)},
       error: (err) => {console.log(err)}
     }));
   }
